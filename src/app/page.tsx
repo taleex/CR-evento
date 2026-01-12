5e53263f7b08ac04b77aba00ebe347f1c392a0dc
Jorge Matos <45945081+taleex@users.noreply.github.com>
2026-01-12 12:13:05 +0000

    metadata dynamic coloring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center px-3 pt-36">
 
       <h1 className="text-3xl lg:text-6xl font-bold tracking-tighter">Find events arround you</h1>
-      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-[#a4f839]">10.000</span> events around you</p>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-accent">10.000</span> events around you</p>
 
       <form className="w-full sm:w-[580px]">
-        <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10" type="text" placeholder="Search events in any city..." spellCheck={false} />
+        <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10" type="text" placeholder="Search events in any city..." spellCheck={false} />
       </form>
 
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
